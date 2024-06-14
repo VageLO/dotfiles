9ea911fb569a4779c41f55dd882ac96256ac72e7
VageLO <48802151+VageLO@users.noreply.github.com>
2024-06-14 10:04:42 +0300

    update: remove default lazy plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,32 +1,21 @@
+require("lua.config.autocmds")
+require("lua.config.keymaps")
+require("lua.config.options")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    -- bootstrap lazy.nvim
-    -- stylua: ignore
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-        lazypath })
+    vim.fn.system({ 
+        "git", 
+        "clone", 
+        "--filter=blob:none", 
+        "https://github.com/folke/lazy.nvim.git", 
+        "--branch=stable",
+        lazypath 
+    })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-    spec = {
-        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-        { import = "plugins" },
-    },
-    checker = { enabled = true, notify = false },
+    spec = "plugins",
     change_detection = { enabled = true, notify = false },
-    performance = {
-        rtp = {
-            -- disable some rtp plugins
-            disabled_plugins = {
-                "gzip",
-                -- "matchit",
-                -- "matchparen",
-                -- "netrwPlugin",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
 })
