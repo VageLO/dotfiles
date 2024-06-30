a6741d21ccc6456346f7343f5e6ba7c7b579c99d
VageLO <48802151+VageLO@users.noreply.github.com>
2024-06-30 19:05:42 +0300

    update: refine & oil.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,24 @@
-require("config.autocmds")
-require("config.options")
-require("config.lazy")
-require("config.keymaps")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "plugins" }
+  },
+  defaults = { lazy = false, version = "*", },
+  change_detection = { enabled = true, notify = false },
+})
---
+++ nvim/plugin/options.lua
@@ -0,0 +1,14 @@
+--vim.g.loaded_netrwPlugin = 0
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.linebreak = true
+opt.scrolloff = 10
+opt.guicursor = "n-v-i-c:block-Cursor"
